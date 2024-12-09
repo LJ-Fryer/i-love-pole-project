6aafec1ef48ab93c213455784e905dd51424ba7b
LJ-Fryer <ljf_aus@hotmail.com>
2024-12-10 00:08:57 +1100

    Reverted back to original

--- js/script.js
+++
@@ -1,6 +0,0 @@
-function changeTheme() {
-  let body = document.querySelector("body");
-  body.classList.toggle("dark");
-}
-let button = document.querySelector("button");
-button.addEventListener("click", changeTheme);
